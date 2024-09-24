7278bc90845ce7a5382a4a845e15091916ebe406
João Manuel <jmsmarques8213@gmail.com>
2024-09-24 16:06:23 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,17 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joao-manuel-s-m.github.io/portfolio/",
+  base: "portfolio",
   integrations: [tailwind()],
   vite: {
     resolve: {
       alias: {
-        '@assets': '../../../public/assets',
-      }
+        "@assets": "../../../public/assets",
+      },
     },
-
   },
   build: {
     minify: false,
   },
-  
 });
